e8bbb294b8bf7b059bc1df6c363b6b4e4fa0d015
Agustin Paturlanne <aguspaturlanne@gmail.com>
2024-06-19 11:24:05 -0300

    fcc finished project

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # URL Shortener Microservice
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+This project is a URL Shortener Microservice, part of the freeCodeCamp.org curriculum. It allows users to input a URL and receive a shortened version of it, making it easier to share and manage long URLs.
+
+## Features
+
+- **URL Shortening:** Users can submit a URL and receive a shortened version of it.
+- **Simple UI:** The service provides a straightforward user interface for easy interaction.
+
+## Technologies Used
+
+- Javascript - Express
+- HTML5
+- CSS3
+
+## How to Use
+
+1. Navigate to the URL Shortener Microservice webpage.
+2. Enter the URL you wish to shorten in the input field labeled "URL".
+3. Click the "POST URL" button.
+4. You will receive a shortened version of your URL.
--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 require('dotenv').config();
+const dns = require('dns');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+//Simulate DB
+let urlDatabase = [];
+
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  let url = req.body.url;
+  let urlObj;
+  try {
+    urlObj = new URL(url);
+  }
+  catch (err) {
+    return res.json({ error: 'invalid url' });
+  }
+  let key = urlDatabase.length + 1;
+  urlDatabase[key] = url;
+  res.json({ original_url: url, short_url: key });
+});
+
+app.get('/api/shorturl/:key', function(req, res) {
+  let short_url = req.params.key;
+  if (!urlDatabase[short_url]) {
+    return res.json({ error: 'No short url found for given input' });
+  }
+  res.redirect(urlDatabase[short_url]);
 });
 
 app.listen(port, function() {
